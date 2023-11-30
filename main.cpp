2a777d241864a7a633620ecf5b4498ba8a474148
Indrayu <149798415+1ndrayu@users.noreply.github.com>
2023-11-30 18:24:45 +0530

    maxAmongNNumbers

--- main.cpp
+++ main.cpp
@@ -1,26 +1,19 @@
-# include <iostream>
+#include <iostream>"
 using namespace std;
+int main()
+{
+    int n=0, a=0, m=0;
+    cout<<"This program will find the max. value among n numbers\n";
+    cout<<"\nEnter the number of values you will enter\n: ";
+    cin>>n;
 
-int main() {
-    int a,b,c,mx=0;
-    cout<<"This program will find the max. value amongst given 3 numbers\n";
-    cout<<"Enter the numbers\n: ";
-    cin>>a>>b>>c;
-
-    if(a>b) {
-        if(a>c)
-            mx=a;
-        else
-            mx=c;
-    }
-    else {
-        if(b>c)
-            mx=b;
-        else
-            mx=c;
+    cout<<"Enter the values\n: ";
+    for(int i=0; i<n; i++) {
+        cin>>a;
+        if(a>m)
+            m=a;
     }
 
-    cout<<"Max Value: "<<mx;
-
-return 0;
+    cout<<"\nMax. value: "<<m;
+    return 0;
 }
